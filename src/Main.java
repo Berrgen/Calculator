887db79f89e306e608ce95d2e00b1d218f01ab07
Berrgen <sergeu.css@gmail.com>
2022-06-01 07:03:39 +0300

    De

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CalculateException{
         Scanner sc = new Scanner(System.in);
         Calculate calc = new Calculate();
         while(true) {
-            try {
                 calc.parser(sc.nextLine());
                 calc.setRes();
                 calc.printResult();
-            } catch (CalculateException e) {
-                System.exit(0);
-            }
         }
 
     }
